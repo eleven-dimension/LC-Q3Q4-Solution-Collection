999dd4d1ccbac70bf0bb7a8e3344d6ad1bacfa91
eleven-dimension <3096152319@qq.com>
2024-09-26 07:48:17 +0800

    Add a crawler to track the number of problems.

--- track_progress.py
+++ track_progress.py
@@ -1,5 +1,25 @@
 import os
 import re
+import requests
+
+MIN_RATING = 1900
+
+def get_problem_number():
+    url = 'https://zerotrac.github.io/leetcode_problem_rating/data.json'
+    result = -1
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json() 
+        left, right = 0, len(data) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if data[mid]['Rating'] > MIN_RATING:
+                result = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+    return result + 1
 
 pattern = re.compile(r'^\d{4}-[a-zA-Z]+(-[a-zA-Z]+)*$')
 
@@ -16,7 +36,8 @@ with open(readme_path, 'r') as file:
     lines = file.readlines()
 
 if lines:
-    lines[-1] = re.sub(r'Progress: \d+ / \d+', f'Progress: {count} / 576', lines[-1])
+    problem_number = get_problem_number()
+    lines[-1] = re.sub(r'Progress: \d+ / \d+', f'Progress: {count} / {problem_number}', lines[-1])
 
 with open(readme_path, 'w') as file:
     file.writelines(lines)
